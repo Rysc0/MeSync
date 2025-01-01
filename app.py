eb8aff4194e868224fe1e07c89380e9ab3f11da7
Rysc0 <boris.bastek@gmail.com>
2025-01-01 19:49:46 +0100

    Updated app.py to render index.html

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 # app.py
-
-from flask import Flask, request
+import flask
+from flask import Flask, request, render_template
 import core
+from flask_cors import CORS
+
 
 # this will do as a temporary database of cards
 # each card ID has list of other cardID's that are mirrors
 DATABASE = {"":""}
 
 app = Flask(__name__)
+CORS(app) #, origins="https://trello.com")
 
 @app.route('/', methods=['POST'])
 def webhook():
@@ -16,5 +19,11 @@ def webhook():
             core.updateCardAction(request.json)
         print("Data received from Webhook is: ", request.json)
         return "Webhook received!"
+    
+    
+@app.route('/', methods=['GET', 'POST'])  # Accepts only POST requests
+def example():
+    return render_template('index.html')
+
 
 app.run(host='0.0.0.0', port=8123)
