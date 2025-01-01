a84aeb90bbc6c20064c0f742ff831d3d367148d8
Rysc0 <boris.bastek@gmail.com>
2025-01-01 22:48:53 +0100

    Updated client.js to get the button popup

--- static/js/client.js
+++ static/js/client.js
@@ -1,8 +1,16 @@
 TrelloPowerUp.initialize({
-  'card-buttons': function(t, options){
-    return [{
-        icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
-        text: 'Estimate Size',
-    }];
+  "card-buttons": function (t, options) {
+    return [
+      {
+        icon: "https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421",
+        text: "Mirror/sync",
+        callback: function (t) {
+          return t.popup({
+            title: "Mirror/sync",
+            url: "test.html",
+          });
+        },
+      },
+    ];
   },
 });
