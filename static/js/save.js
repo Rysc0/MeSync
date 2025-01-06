2689e7fe8aa2fc3e3d02df5c65c787a442689b21
Rysc0 <boris.bastek@gmail.com>
2025-01-06 12:32:54 +0100

    Updated save.js to prepare for fetching lists

--- static/js/save.js
+++ static/js/save.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const dropdownList = document.getElementById('listDropdown');
  
   // Function to fetch data and populate the dropdown
-  async function fetchData() {
+  async function fetchBoardsData() {
     await fetch('http://127.0.0.1:8123/getBoards') // Replace with your API URL
       .then(response => {
         if (!response.ok) {
@@ -18,7 +18,24 @@ document.addEventListener("DOMContentLoaded", function () {
         return response.json();
       })
       .then(data => {
-        populateDropdown(data);
+        populateBoardsDropdown(data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }
+
+  async function fetchListsData() {
+    await fetch('http://127.0.0.1:8123/getFilteredListsOnBoard') // Replace with your API URL
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        //console.log(response.json())
+        return response.json();
+      })
+      .then(data => {
+        populateListsDropdown(data);
       })
       .catch(error => {
         console.error('Error fetching data:', error);
@@ -26,10 +43,8 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
 
-
-
   // Function to populate dropdown
-  async function populateDropdown(data) {
+  async function populateBoardsDropdown(data) {
       // generate options based on the response
       for (i = 0, len = data.length; i < len; i++) {
         const option = document.createElement('option');
@@ -37,6 +52,17 @@ document.addEventListener("DOMContentLoaded", function () {
         option.text = data[i].name;
         dropdown.appendChild(option);
       } 
+  }
+    
+  async function populateListsDropdown(data) {
+    // generate options based on the response
+    for (i = 0, len = data.length; i < len; i++) {
+      const option = document.createElement('option');
+      option.value = data[i].id; // Adjust based on API response structure
+      option.text = data[i].name;
+      dropdownList.appendChild(option);
+    } 
+  }
       // const option = document.createElement('option');
       // option.value = 'Diplomski'; // Adjust based on API response structure
       // option.text = 'Diplomski';
@@ -55,12 +81,13 @@ document.addEventListener("DOMContentLoaded", function () {
       // listOption_2.value = 'TO DO';
       // listOption_2.text = 'TO DO';
       // dropdownList.appendChild(listOption_2)
-  }
-
-
-  // Call fetch function on load
-  fetchData();
+      // Call fetch function on load
+      fetchBoardsData();
+      fetchListsData();
 });
+
+
+
 
 
 
