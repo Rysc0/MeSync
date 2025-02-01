79e39324777b5b64cb1494750821179b3f274ac8
Rysc0 <boris.bastek@gmail.com>
2025-02-01 21:01:19 +0100

    Removed unused route

--- app.py
+++ app.py
@@ -12,15 +12,7 @@ DATABASE = {"":""}
 app = Flask(__name__)
 CORS(app) #, origins="https://trello.com")
 
-@app.route('/', methods=['POST'])
-def webhook():
-    if request.method == 'POST':
-        if request.json["action"]["type"] == "updateCard":
-            core.updateCardAction(request.json)
-        print("Data received from Webhook is: ", request.json)
-        return "Webhook received!"
-    
-    
+
 @app.route('/', methods=['GET', 'POST'])  # Accepts only POST requests
 def example():
     return render_template('index.html')
