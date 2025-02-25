4e0883e4c1aec90ee3ef0915881cefbac19b1b18
Rysc0 <boris.bastek@gmail.com>
2025-02-25 22:26:55 +0100

    Added routes.py

---
+++ routes.py
@@ -0,0 +1,40 @@
+from flask import request, render_template
+import core
+
+
+
+def register_routes(app, db):
+
+    @app.route('/', methods=['GET', 'POST'])  # Accepts only POST requests
+    def example():
+        core.get_db_user()
+        return render_template('index.html')
+
+    @app.route('/test', methods=['GET', 'POST'])  # Accepts only POST requests
+    def test():
+        return render_template('test.html')
+
+    @app.route('/getBoards', methods=['GET'])  # Accepts only POST requests
+    def getBoards():
+        return core.getBoards()
+
+
+    @app.route('/getFilteredListsOnBoard', methods=['GET'])  # Accepts only POST requests
+    def getFilteredListOnBoard():
+        boardID = request.args.get('boardID')
+        filter = 'open'
+        return core.getFilteredListsOnBoard(boardID, filter)
+
+
+    @app.route('/createMirrorCard', methods=['POST'])
+    def createMirrorCard():
+        req = request.get_json()
+        idList = req["listID"]
+        idCardSource = req["originalCardID"]
+        return core.createMirrorCard(listID=idList, idCardSource=idCardSource)
+
+
+    @app.route('/webhook', methods=['POST'])
+    def receiveChange():
+        req = request.get_json()
+        return core.syncronizeCards(req)
