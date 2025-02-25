06cdce4048b045680ea5817e13680f9c1f55ff30
Rysc0 <boris.bastek@gmail.com>
2025-02-25 22:26:00 +0100

    Changed app.py to use factory pattern

--- app.py
+++ app.py
@@ -1,52 +1,65 @@
 # app.py
 import flask
 from flask import Flask, request, render_template
-import core
+#import core
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
+db = SQLAlchemy()
 
-# this will do as a temporary database of cards
-# each card ID has list of other cardID's that are mirrors
-DATABASE = {"":""}
+def create_app():
+    app = Flask(__name__, template_folder='templates')
+    CORS(app)  # , origins="https://trello.com")
+    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+    db.init_app(app)
+
+    # imports
+    from routes import register_routes
+    register_routes(app, db)
+
+    migrate = Migrate(app, db)
+
+    return app
+
 
 app = Flask(__name__)
 CORS(app) #, origins="https://trello.com")
 
-
-@app.route('/', methods=['GET', 'POST'])  # Accepts only POST requests
-def example():
-    return render_template('index.html')
-
-@app.route('/test', methods=['GET', 'POST'])  # Accepts only POST requests
-def test():
-    return render_template('test.html')
-
-@app.route('/getBoards', methods=['GET'])  # Accepts only POST requests
-def getBoards():
-    return core.getBoards()
-
-
-@app.route('/getFilteredListsOnBoard', methods=['GET'])  # Accepts only POST requests
-def getFilteredListOnBoard():
-    boardID = request.args.get('boardID')
-    filter = 'open'
-    return core.getFilteredListsOnBoard(boardID, filter)
-
-
-@app.route('/createMirrorCard', methods=['POST'])
-def createMirrorCard():
-    req = request.get_json()
-    idList = req["listID"]
-    idCardSource = req["originalCardID"]
-    return core.createMirrorCard(listID=idList, idCardSource=idCardSource)
-
-
-@app.route('/webhook', methods=['POST'])
-def receiveChange():
-    req = request.get_json()
-    return core.syncronizeCards(req)
-
-
-app.run(host='0.0.0.0', port=8123, debug=True)
+# @app.route('/', methods=['GET', 'POST'])  # Accepts only POST requests
+# def example():
+#     return render_template('index.html')
+#
+# @app.route('/test', methods=['GET', 'POST'])  # Accepts only POST requests
+# def test():
+#     return render_template('test.html')
+#
+# @app.route('/getBoards', methods=['GET'])  # Accepts only POST requests
+# def getBoards():
+#     return core.getBoards()
+#
+#
+# @app.route('/getFilteredListsOnBoard', methods=['GET'])  # Accepts only POST requests
+# def getFilteredListOnBoard():
+#     boardID = request.args.get('boardID')
+#     filter = 'open'
+#     return core.getFilteredListsOnBoard(boardID, filter)
+#
+#
+# @app.route('/createMirrorCard', methods=['POST'])
+# def createMirrorCard():
+#     req = request.get_json()
+#     idList = req["listID"]
+#     idCardSource = req["originalCardID"]
+#     return core.createMirrorCard(listID=idList, idCardSource=idCardSource)
+#
+#
+# @app.route('/webhook', methods=['POST'])
+# def receiveChange():
+#     req = request.get_json()
+#     return core.syncronizeCards(req)
+#
+#
+# app.run(host='0.0.0.0', port=8123, debug=True)
 
 
